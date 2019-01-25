8d2a092135e16a4dd873be43406dde2c3fefca66
Ben <bstanfie@usc.edu>
2019-01-25 12:03:33 -0800

    added vars, mixins, and updated about page

--- proj/app/initialize.js
+++ proj/app/initialize.js
@@ -1,4 +1,55 @@
 document.addEventListener('DOMContentLoaded', () => {
   // do your setup here
   var $ = require('jquery');
+  function countdown(endDate) {
+    let days, hours, minutes, seconds;
+    
+    endDate = new Date(endDate).getTime();
+    
+    if (isNaN(endDate)) {
+    return;
+    }
+    
+    setInterval(calculate, 1000);
+    
+    function calculate() {
+      let startDate = new Date();
+      startDate = startDate.getTime();
+      
+      let timeRemaining = parseInt((endDate - startDate) / 1000);
+      
+      if (timeRemaining >= 0) {
+        days = parseInt(timeRemaining / 86400);
+        timeRemaining = (timeRemaining % 86400);
+        
+        hours = parseInt(timeRemaining / 3600);
+        timeRemaining = (timeRemaining % 3600);
+        
+        minutes = parseInt(timeRemaining / 60);
+        timeRemaining = (timeRemaining % 60);
+        
+        seconds = parseInt(timeRemaining);
+        
+        // Populates counter
+        document.getElementById("days").innerHTML = `${parseInt(days, 10)} DAYS.`;
+        document.getElementById("hours").innerHTML = `${parseInt(hours, 10)} HOURS.`;
+      } else {
+        return;
+      }
+    }
+  }
+
+  countdown('02/07/2019 11:59:59 PM'); 
+
+  $(document).ready(function() {
+      var options = {
+          strings: ['engineering', 'design', 'tech', 'new'],
+          typeSpeed: 20,
+          startDelay: 500,
+          smartBackspace: true,
+          backspeed: 1000,
+          backDelay: 5000
+      }
+      var typed = new Typed(".element", options);
+  });
 });
