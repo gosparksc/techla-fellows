872c189001f0665cad16c31a3d0a02d2e47f1ed8
Ben <bstanfie@usc.edu>
2019-01-27 09:36:44 -0800

    tidying up images and pug-ifying index

--- proj/app/initialize.js
+++ proj/app/initialize.js
@@ -46,7 +46,6 @@ document.addEventListener('DOMContentLoaded', () => {
           typeSpeed: 20,
           startDelay: 500,
           smartBackspace: true,
-          backspeed: 1000,
           backDelay: 5000
       }
       var typed = new Typed(".element", options);
